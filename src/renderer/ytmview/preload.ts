b2990c2dd809a4dd1c8ea14debb5e229199f3922
NovusTheory <3434404+NovusTheory@users.noreply.github.com>
2025-03-19 12:07:07 -0500

    🐛 Fix always show history button override
    YTM changed the element name and broke the selector

--- src/renderer/ytmview/preload.ts
+++ src/renderer/ytmview/preload.ts
@@ -176,7 +176,7 @@ async function hookPlayerApiEvents() {
 
 function overrideHistoryButtonDisplay() {
   // @ts-expect-error Style is reported as readonly but this still works
-  document.querySelector<HTMLElement>("#history-link tp-yt-paper-icon-button").style = "display: inline-block !important;";
+  document.querySelector<HTMLElement>("#history-link .history-button").style = "display: inline-block !important;";
 }
 
 function getYTMTextRun(runs: { text: string }[]) {
