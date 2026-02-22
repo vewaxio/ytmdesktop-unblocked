8903b61e5eb32d51d6b2c39b8494cf6acad3d6e2
Vewaxio <49758078+vewaxio@users.noreply.github.com>
2026-02-22 18:24:15 +0700

    fix: use correct asar.unpack for adblocker preload

--- forge.config.ts
+++ forge.config.ts
@@ -20,7 +20,9 @@ const config: ForgeConfig = {
   packagerConfig: {
     executableName: "youtube-music-desktop-app",
     icon: "./src/assets/icons/ytmd",
-    asarUnpack: ["**/node_modules/@cliqz/adblocker-electron-preload/**"],
+    asar: {
+      unpack: "**/node_modules/@cliqz/adblocker-electron-preload/**"
+    },
     extraResource: [
       "./src/assets/icons/tray.ico",
       "./src/assets/icons/trayTemplate.png",
@@ -40,8 +42,7 @@ const config: ForgeConfig = {
         schemes: ["ytmd"]
       }
     ],
-    appCategoryType: "public.app-category.music",
-    asar: true
+    appCategoryType: "public.app-category.music"
   },
   rebuildConfig: {},
   makers: [
